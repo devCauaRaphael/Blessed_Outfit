81028b7dac202255349597049fbb93a3c5af8b3a
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2024-12-14 14:01:13 -0300

    Adicionado as setas de ir para cima e para baixo no site

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,51 @@ cartButtonList.forEach((button) => {
        
     });
 });
+
+
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoVoltarAoTopo = document.createElement('button');
+    const botaoIrParaBase = document.createElement('button');
+
+    // Definir classes para estilização com Bootstrap
+    botaoVoltarAoTopo.className = 'btn btn-primary position-fixed rounded-circle';
+    botaoVoltarAoTopo.innerHTML = '<i class="bi bi-arrow-up"></i>';
+    botaoVoltarAoTopo.style.backgroundColor = 'black';
+    botaoVoltarAoTopo.style.color = 'white';
+    botaoVoltarAoTopo.style.border = '2px solid #f9f9f9';
+    botaoVoltarAoTopo.style.bottom = '80px';
+    botaoVoltarAoTopo.style.right = '20px';
+    botaoVoltarAoTopo.style.display = 'none'; // Escondido por padrão
+
+    botaoIrParaBase.className = 'btn btn-primary position-fixed rounded-circle';
+    botaoIrParaBase.innerHTML = '<i class="bi bi-arrow-down"></i>';
+    botaoIrParaBase.style.backgroundColor = 'black';
+    botaoIrParaBase.style.color = 'white';
+    botaoIrParaBase.style.border = '2px solid #f9f9f9';
+    botaoIrParaBase.style.bottom = '20px';
+    botaoIrParaBase.style.right = '20px';
+
+    document.body.appendChild(botaoVoltarAoTopo);
+    document.body.appendChild(botaoIrParaBase);
+
+    // Funcionalidade para voltar ao topo
+    botaoVoltarAoTopo.addEventListener('click', function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    // Funcionalidade para ir para a base da página
+    botaoIrParaBase.addEventListener('click', function () {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    });
+
+    // Mostrar ou esconder o botão de voltar ao topo com base na posição de rolagem
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 100) {
+            botaoVoltarAoTopo.style.display = 'block';
+        } else {
+            botaoVoltarAoTopo.style.display = 'none';
+        }
+    });
+});
+
